96fe4c0fc886bdb5681b9ebf8329dfe67e5ea098
Bruno <brunoborcato97@gmail.com>
2018-05-12 21:21:10 -0300

    ADD function subtração

--- js/calc.js
+++ js/calc.js
@@ -43,6 +43,24 @@ var borrar = false;
  		}
  		borrar = true;
 	}
+	function subtracao(o){
+		var caixa1 = document.f1.txtcaixa1.value;
+		var caixa2 = document.f1.txtcaixa2.value;
+		var unum = caixa1.substring(caixa1.length-1);
+		calcular()
+		if (unum == "-") {
+			unum = unum.replace(unum,o);
+			var res = caixa1.substring(0,caixa1.length-1);
+			document.f1.txtcaixa1.value = res-unum;
+		}
+		if (caixa1 == "" && caixa2 != ""){
+			document.f1.txtcaixa1.value = caixa2 + o;
+		}
+		else{
+			document.f1.txtcaixa1.value = caixa1 - caixa2 - o;
+ 		}
+ 		borrar = true;
+	}
 	function calcular(){
 		var caixa1 = document.f1.txtcaixa1.value;
 		var caixa2 = document.f1.txtcaixa2.value;
@@ -51,7 +69,7 @@ var borrar = false;
 		borrar = true;
 		blocdel = true;
 	}
-	function masmenos(){
+	function maismenos(){
 		var caixa2 = document.f1.txtcaixa2.value;
 		if (caixa2 > 0){
 			document.f1.txtcaixa2.value = "(-" + caixa2 + ")";
